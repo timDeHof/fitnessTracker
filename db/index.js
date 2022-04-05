7d9e786a070879753d9b208f140ca1d9cf9f969c
Chudi <chudionubogu@yahoo.com>
2022-04-05 13:26:05 -0500

    quicksave

--- db/index.js
+++ db/index.js
@@ -1,1 +1,10 @@
 // require and re-export all files in this db directory (users, activities...)
+
+const { client } = require("pg");
+
+module.exports = {
+  ...require("./users"), // adds key/values from users.js
+  ...require("./activities"), // adds key/values from activites.js
+  ...require("./routines"), // etc
+  ...require("./routine_activities"), // etc
+};
