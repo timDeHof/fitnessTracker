52c6acde09dcc7d016e487c630687448606156ad
Chudi <chudionubogu@yahoo.com>
2022-04-07 10:10:09 -0500

    i fucked up

--- db/index.js
+++ db/index.js
@@ -6,5 +6,5 @@ module.exports = {
   ...require("./users"), // adds key/values from users.js
   ...require("./activities"), // adds key/values from activites.js
   ...require("./routines"), // etc
-  //...require("./routine_activities"), // etc
+  ...require("./routine_activities"), // etc
 };
