62580486d006fb4dce16d4518429426cbd08b305
Chudi <chudionubogu@yahoo.com>
2022-04-08 14:30:24 -0500

    branch for api users

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-// create an api router
-// attach other routers from files in this api directory (users, activities...)
-// export the api router
 const express = require("express");
 const apiRouter = express.Router();
+const { JWT_SECRET } = process.env;
+
+apiRouter.get("/health", (req, res) => {
+  res.send({ message: "String" });
+});
+
+const userRouter = require("./users");
+apiRouter.use("/users", userRouter);
+//apiRouter.use("/activities", activities);
+//apiRouter.use("/routines", routines);
+//apiRouter.use("/routineActivity", routineActivity);
 
 module.exports = apiRouter;
---
+++ api/users.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+const {
+  createUser,
+  getUser,
+  getUserById,
+  getUserByUsername,
+} = require("../db/users");
+
+userRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log("here is the req.body", req.body);
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+    if (password.length > 8) {
+      next({
+        name: "PasswordLengthError",
+        message:
+          "Password is too short, please type in 8 at least 8 characters",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = userRouter;
+
+//Dummy Usernames/Password Below
