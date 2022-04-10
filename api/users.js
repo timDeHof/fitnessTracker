a543bc59192df32720cd9e22f4a3fc7382ebf94a
timDeHof <this.is.tim.dehof@gmail.com>
2022-04-10 16:51:26 -0400

    removed brackets around '_user' and it works?? through thunder client

--- api/users.js
+++ api/users.js
@@ -12,45 +12,53 @@ const { token } = require("morgan");
 
 const bodyParser = require("body-parser");
 userRouter.use(bodyParser.json());
+
 userRouter.post("/register", async (req, res, next) => {
   //console.log("req:", req);
   const { username, password } = req.body;
 
   console.log("here is the req.body", req.body);
+  // const token = jwt.sign({ username, password }, process.env.JWT_SECRET);
+  // res.send({
+  //   message: "thanks for signing Up!",
+  //   token: token,
+  // });
+  console.log("password.length:", password.length);
   try {
-    // const _user = await getUserByUsername(newUser);
-    // console.log(_user);
-    // console.log("The datatype of _user is ", typeof { _user });
-    // if ({ _user }) {
-    //   next({
-    //     name: "UserExistsError",
-    //     message: "A user by that username already exists",
-    //   });
-    //}
-    if (password.length > 8) {
+    const _user = await getUserByUsername(username);
+    console.log("_user:", _user);
+    console.log("The datatype of _user is ", typeof { _user });
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+    if (password.length < 8) {
       next({
         name: "PasswordLengthError",
         message:
           "Password is too short, please type in 8 at least 8 characters",
       });
-    }
-    console.log("datatype of username:", typeof req.body);
-    const registeredUser = await createUser(req.body);
-    console.log("datatype of registeredUser:", typeof registeredUser);
-    console.log("registered user:", registeredUser);
-    const token = jwt.sign(
-      { id: registeredUser.id, username: registeredUser.username },
-      process.env.JWT_SECRET,
-      { expiresIn: "1w" }
-    );
-    console.log("token:", token);
+    } else {
+      console.log("datatype of username:", typeof req.body);
+      const registeredUser = await createUser(req.body);
+      console.log("datatype of registeredUser:", typeof registeredUser);
+      console.log("registered user:", registeredUser);
+      const token = jwt.sign(
+        { id: registeredUser.id, username: registeredUser.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1w" }
+      );
+      console.log("token:", token);
 
-    res.json({
-      message: "thank you for signing up",
-      token: token,
-    });
-    //res.send({ registeredUser });
-    //req.end();
+      return res.send({
+        message: "thank you for signing up",
+        token: token,
+      });
+
+      //res.send({ registeredUser });
+    }
   } catch ({ name, message }) {
     next({ name, message });
   }
